a1397a2e7ec07b5b9daae00b764bc01623952be4
Abhishek Dokania <abhishekdokania@Abhisheks-MacBook-Pro.local>
2018-05-16 11:22:13 -0400

    initial project version

---
+++ src/main/scala/HelloWorld/Hello_one.scala
@@ -0,0 +1,23 @@
+package HelloWorld
+
+object Hello_one {
+  def main(args:Array[String]) {
+    // val name:String = args(0)
+    val name:String = "World"
+    println("Hello " + name + "!!")
+  }
+}
+
+import java.util.{Date, Locale}
+import java.text.DateFormat
+import java.text.DateFormat._
+
+object PrintDate {
+  def main(args: Array[String]): Unit = {
+    val now =new Date
+    val df = getDateInstance(LONG,Locale.US)
+    println(df.format(now))
+  }
+}
+
+
